11ec0803bca69de34944795b1c971141e2715291
Dilday <ben.dilday.phd@gmail.com>
2017-01-25 22:36:55 -0500

    add sqlite option

--- create.py
+++ create.py
@@ -3,31 +3,61 @@ import sys
 import warnings
 
 import pymysql as mdb
+import sqlite3
 
 from tt import *
 
 warnings.filterwarnings('ignore')
 
 
+def create_table_command_mysql():
+    sql_commands = [('''
+    use {db};'.format(args_dict['db']);
+    create table if not exists {table} (
+        id int not null auto_increment,
+        date varchar(10),
+        start time(0),
+        end time(0),
+        project varchar(8),
+        primary key (id)
+    );
+    ;
+    '''.format(db=args_dict['db'], table=args_dict['table']))]
+
+    sql_commands += ['create database if not exists {}'.format(args_dict['db'])]
+    return sql_commands
+
+def create_table_command_sqllite():
+    sql_commands = [('''
+    create table if not exists {table} (
+        id int auto increment,
+        date varchar(10),
+        start time(0),
+        end time(0),
+        project varchar(8),
+        primary key (id)
+    );
+    '''.format(table=args_dict['table']))]
+
+    return sql_commands
+
 def run(args_dict):
     args_dict = update_args(args_dict)
 
-    db = mdb.connect(host='{}'.format(args_dict['host']), user='{}'.format(USERNAME),
-                     password='{}'.format(PASSWORD), autocommit=True)
-    sql = ('''
-        use {db};
-        create table {table} (
-            id int not null auto_increment,
-            date varchar(10),
-            start time(0),
-            end time(0),
-            project varchar(8),
-            primary key (id)
-        );
-        insert into {table} (date, start, end, project) values ('01/01/2016', '0:00', '1:00', 'test');
-    '''.format(db=args_dict['db'], table=args_dict['table']))
-    db.cursor().execute('create database if not exists {}'.format(args_dict['db']))
-    db.cursor().execute(sql)
+    if args_dict['dbengine'] == 'mysql':
+        db = mdb.connect(host='{}'.format(args_dict['host']),
+                         user='{}'.format(USERNAME),
+                         password='{}'.format(PASSWORD),
+                         autocommit=True)
+        sql_commands = create_table_command_mysql()
+    elif args_dict['dbengine'] == 'sqlite':
+        db = sqlite3.connect('{}.db'.format(args_dict['db']))
+        sql_commands = create_table_command_sqllite()
+    else:
+        raise ValueError, 'dbengine: {} not known'.format(args_dict['dbengine'])
+
+    for sql in sql_commands:
+        db.cursor().execute(sql)
     db.cursor().close()
     db.close()
 
@@ -40,6 +70,8 @@ if __name__ == '__main__':
                         'create.')
     parser.add_argument('--host', required=False, help='Database host; will default to '
                         'config settings.')
+    parser.add_argument('--dbengine', required=False, help='Database engine; will default to '
+                        'config settings. Currently, mysql and postgres are supported')
     args_dict = vars(parser.parse_args())
 
     run(args_dict)
