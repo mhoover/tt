52108aad5ad2cc5c986d31ce0a6ba0aa4490aa2e
Dilday <ben.dilday.phd@gmail.com>
2017-01-26 10:51:00 -0500

    read all fields defined in config file into dictionary

--- __init__.py
+++ __init__.py
@@ -9,9 +9,8 @@ def update_args(args):
     cfg = ConfigParser.ConfigParser()
     cfg.read('config.cfg')
 
-    for field in ['host', 'db', 'table']:
-        if not args[field]:
-            args[field] = cfg.get('database', field)
-    args['gnuplot'] = cfg.get('database', 'gnuplot')
+    for field, field_value in cfg.items('database'):
+        if (field not in args) or (args[field] is None):
+            args[field] = field_value
 
     return args
